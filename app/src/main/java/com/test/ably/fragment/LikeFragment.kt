ffa295c31006fd1293c9602c6b89621e7645d4ef
ChoiJongmun <jmchoi@rsupport.com>
2021-06-22 19:47:49 +0900

    Like 페이지 작업 완료

--- app/src/main/java/com/test/ably/fragment/LikeFragment.kt
+++ app/src/main/java/com/test/ably/fragment/LikeFragment.kt
@@ -6,13 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.test.ably.MainActivity
+import com.test.ably.R
+import com.test.ably.adapter.GoodsAdapter
 import com.test.ably.databinding.FragmentLikeBinding
 import com.test.ably.di.factory.MainViewModelFactory
+import com.test.ably.model.Goods
+import com.test.ably.util.GridSpacingItemDecoration
 import com.test.ably.viewmodel.MainViewModel
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
-class LikeFragment : DaggerFragment() {
+class LikeFragment : DaggerFragment(), View.OnClickListener {
 
     @Inject
     lateinit var factory : MainViewModelFactory
@@ -26,7 +30,37 @@ class LikeFragment : DaggerFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLikeBinding.inflate(inflater, container, false)
+
+        binding.lifecycleOwner = activity as MainActivity
+
         viewModel = ViewModelProvider(activity as MainActivity, factory).get(MainViewModel::class.java)
+
+        var adapter = GoodsAdapter(this)
+
+        binding.rvLike.adapter = adapter
+
+        val spanCount = 2
+        val spacing = 50
+        val inCludeEdge = false
+        binding.rvLike.addItemDecoration(GridSpacingItemDecoration(spanCount, spacing, inCludeEdge))
+
+        viewModel.goods.observe(activity as MainActivity){
+            adapter.submitList(it.filter { it.like == true }.toMutableList())
+        }
+
+
         return binding.root
     }
+
+    override fun onClick(v: View?) {
+        if(v != null) {
+            when (v.id) {
+                R.id.iv_like -> {
+                    val item = v.tag as Goods
+                    viewModel.setLikeData(item.id)
+                }
+            }
+        }
+    }
+
 }
